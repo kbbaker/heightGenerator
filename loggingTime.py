25ffd4dd04cb66279894414630de780b81af7fe2
kbbaker <kevin.baker@routeyou.com>
2016-12-16 15:54:29 +0100

    logging example

---
+++ loggingTime.py
@@ -0,0 +1,20 @@
+__author__ = 'Kevin'
+import time
+import random
+
+
+start = time.time()
+
+#your algo here
+##
+##
+##
+for i in range(0,10):
+    time.sleep(random.random())
+##
+##
+##
+
+end = time.time()
+
+print "your algorithm took " + str(end - start) + "seconds"
