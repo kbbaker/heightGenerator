401833dcbe9199b243f995f09797414ca0e97ca7
kbbaker <kevin.baker@routeyou.com>
2016-12-16 15:43:10 +0100

    added try/except loop in function

--- srtmCall.py
+++ srtmCall.py
@@ -4,18 +4,38 @@ import urllib2
 import xml.etree.ElementTree as ET
 import sys
 import json
+import time
 
 def fetch_result(url):
-    #fetch result from route json#
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
-    return json.load(response)
+    count = 0
+    while True:
+        try:
+            #fetch result from route json#
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            json_response = json.load(response)
+            if 'error' in json_response:
+                print "error in respons: " + json_response['error']
+                print "retry after 5 seconds"
+                time.sleep(5)
+            else:
+                return json_response
+        except:
+            #state how many loops you want. In this example 10
+            if count < 10:
+                count += 1
+                print "failed to fetch respons -- retry after 5 seconds: " + str(count)
+                time.sleep(5)
+            else:
+                print "failed to fetch respons"
+                return {'result': None}
 
-key = 'geef hier jouw key in'
+
+key = 'xxxxxxxxxxxxxxxx'
 
 params = [key]
 
-url = 'http://api.routeyou.com/2.0/json/Session?id=1&method=start&params=' + json.dumps(params)
+url = 'http://api.routeyou.com/3.0/json/Session?id=1&method=start&params=' + json.dumps(params)
 
 token = fetch_result(url)['result']
 
